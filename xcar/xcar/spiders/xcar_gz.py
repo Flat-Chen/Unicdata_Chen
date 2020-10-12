424de73d2cb6a767a02ddb27f65f520780dfcfb6
Flat-Chen <1316446041@qq.com>
2020-10-12 18:11:16 +0800

    commit

--- xcar/xcar/spiders/xcar_gz.py
+++ xcar/xcar/spiders/xcar_gz.py
@@ -19,7 +19,7 @@ car_msg_df_new = car_msg_df.drop_duplicates('vehicle_id')
 
 
 # 随机生成一个手机号
-def phoneNORandomGenerator(self):
+def phoneNORandomGenerator():
     prelist = ["130", "131", "132", "133", "134", "135", "136", "137", "138", "139", "147", "150", "151", "152", "153",
                "155", "156", "157", "158", "159", "186", "187", "188"]
     return random.choice(prelist) + "".join(random.choice("0123456789") for i in range(8))
@@ -40,7 +40,7 @@ class XcarGzSpider(scrapy.Spider):
         super(XcarGzSpider, self).__init__(**kwargs)
         self.counts = 0
         self.car_msg_df_new = car_msg_df_new
-        self.city_list = [{'province': '1', 'city_id': '475', 'city_name': '北京市'},
+        self.city_list = [{'province': '1', 'city_id': '475', 'city_name': '%e5%8c%97%e4%ba%ac%e5%b8%82'},
                           {'province': '2', 'city_id': '507', 'city_name': '上海市'},
                           {'province': '30', 'city_id': '347', 'city_name': '广州市'},
                           {'province': '17', 'city_id': '386', 'city_name': '成都市'}]
@@ -74,32 +74,62 @@ class XcarGzSpider(scrapy.Spider):
                     for year in year_list:
                         if year == localyears:
                             month = localmonth - 1
-                            month = f"0{str(month)}" if month < 10 else month
+                            # month = f"0{str(month)}" if month < 10 else month
                             mile = '0_1'
-                            regDate = str(year) + str(month)
+                            regDate = str(year) + '-' + str(month) + '-' + '1'
                             data = {
-                                'mid': '27491',
-                                'province_id': '1',
-                                'city_id': '475',
-                                'city_name': '',
-                                'card_time': '',
-                                'mileage': '2',
-                                'mobile': phoneNORandomGenerator
+                                'mid': vehicle_id,
+                                'province_id': province,
+                                'city_id': city_id,
+                                'city_name': city_name,
+                                'card_time': regDate,
+                                'mileage': mile,
+                                'mobile': phoneNORandomGenerator()
                             }
-                            yield scrapy.Request(url=url, meta={"info": (vehicle_id, regDate, mile, city)})
+                            data = json.dumps(data)
+                            print(data)
+                            yield scrapy.Request(url=url, method='POST', body=data,
+                                                 meta={"info": (vehicle_id, regDate, mile, city_name)})
                         else:
                             month = localmonth
-                            month = f"0{str(month)}" if month < 10 else month
+                            # month = f"0{str(month)}" if month < 10 else month
                             mile = (localyears - year) * 2
-                            regDate = str(year) + str(month)
-                            yield scrapy.Request(url=url, meta={"info": (vehicle_id, regDate, mile, city)})
+                            regDate = str(year) + '-' + str(month) + '-' + '1'
+                            data = {
+                                'mid': vehicle_id,
+                                'province_id': province,
+                                'city_id': city_id,
+                                'city_name': city_name,
+                                'card_time': regDate,
+                                'mileage': mile,
+                                'mobile': phoneNORandomGenerator()
+                            }
+                            data = json.dumps(data)
+                            print(data)
+                            yield scrapy.Request(url=url, method='POST', body=data,
+                                                 meta={"info": (vehicle_id, regDate, mile, city_name)})
                 else:
                     year = localyears
                     month = localmonth - 1
-                    month = f"0{str(month)}" if month < 10 else month
-                    regDate = str(year) + str(month)
+                    # month = f"0{str(month)}" if month < 10 else month
+                    regDate = str(year) + '-' + str(month) + '-' + '1'
                     mile = '0_1'
-                    yield scrapy.Request(url=url, meta={"info": (vehicle_id, regDate, mile, city)})
+                    data = {
+                        'mid': vehicle_id,
+                        'province_id': province,
+                        'city_id': city_id,
+                        'city_name': city_name,
+                        'card_time': regDate,
+                        'mileage': mile,
+                        'mobile': phoneNORandomGenerator()
+                    }
+                    data = json.dumps(data)
+                    print(data)
+                    yield scrapy.Request(url=url, method='POST', body=data,
+                                         meta={"info": (vehicle_id, regDate, mile, city_name)})
+                break
+            break
 
     def parse(self, response):
-        pass
+        json_data = json.loads(response.text)
+        print(json_data)
