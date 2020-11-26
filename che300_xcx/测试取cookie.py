fb935ae18ed385979312365db5f6b71bc67ab6e0
Flat-Chen <1316446041@qq.com>
2020-11-26 17:18:36 +0800

    commit

--- che300_xcx/测试取cookie.py
+++ che300_xcx/测试取cookie.py
@@ -1,44 +1,101 @@
-import requests
+# import requests
+#
+# # from requests_html import HTMLSession
+#
+# session = requests.session()
+#
+#
+# def getProxy():
+#     # 设置代理
+#     s = requests.session()
+#     s.keep_alive = False
+#
+#     url = 'http://192.168.2.120:5000'
+#     headers = {
+#         'Connection': 'close',
+#     }
+#     proxy = s.get(url=url, headers=headers, auth=('admin', 'zd123456')).text[0:-6]
+#     return proxy
+#
+#
+# proxy = getProxy()
+# headers = {
+#     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+#     'Accept-Encoding': 'gzip, deflate, br',
+#     'Accept-Language': 'zh-CN,zh;q=0.9',
+#     'Connection': 'keep-alive',
+#     'Cookie': '_che300=SOoVQaNf4%2BsEkXOh6Av%2BbS1FgZNx1OeY%2F8JA77%2B07xEmpi6s5k1uJG1lU5jmXrbTnBcxYCdzlu22CsfL0XQvP9%2BDLX30gtWePrSkTAE6cVBy6U3a%2FC9e%2BpnzfR4KjRmqkQOZNaDyUzNN66dIdP5dPIv63L23pkX9JO%2FGfuNVJogywprwjBv%2Bf9d9S%2BmXxuXUTcNM2km6sFWVu71rIr%2F0aR2vHJ9pBnw4T6NhvtMtiUd2VE96kKXBh%2FYmW7sXyyFhGChNeDsIKdlEQj8LHjyoGas6u9S5bjRnlSXWFGwgNA%2BzwnlqIos2Uyg39kSt1bgVZPbJq2uozTMGIqw7smO9AuokOkMbWFyjutXHyz74%2Ffmc37JzL64MYiWiV5RhJBtocT%2B9VEH0mscO5cIp0f9BkBaV2rNsrOksehWR4T%2BpRIv%2FPWzLn5B1skB64LunzffgfLRy%2BC2jmHIYQ6aRwAGlzwxwZZLflGJr8KQdn7qZJrAPSiP8lWgZuzzd%2FCbYAKnXRm7YlJ59v82iAjJH2lf7Cw9qApOTU3xnuQths11l7w4rTYW4prlnM6AAQHoMVypm578vWH8MAa%2F4W%2F5YPfuk0A%3D%3Dd6d75f981ac3c966f4e66b811fbee19be221018d; device_id=; tel=16534186105; pcim=84ceb450bcd9214ab061669659bac1fc54ba4dde; spidercooskieXX12=1606211385; spidercodeCI12X3=804b84067d9b5ec7daa7a2685890802a',
+#     'Host': 'm.che300.com',
+#     'Referer': 'https://m.che300.com/estimate/result/3/3/1/1/1146060/2019-3/2/1/null/2020/2018?rt=1605495570129',
+#     'Sec-Fetch-Dest': 'document',
+#     'Sec-Fetch-Mode': 'navigate',
+#     'Sec-Fetch-Site': 'same-origin',
+#     'Upgrade-Insecure-Requests': '1',
+#     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+# }
+# url = 'http://m.che300.com/estimate/result/3/3/1/1/1146060/2019-3/2/1/null/2020/2018'
+# # url = 'http://www.baidu.com'
+# response = session.get(url=url, headers=headers, proxies={'http': '81.68.214.148:16128'})
+# # response.html.render()
+# # print(response.cookies.get_dict())
+# print(response.headers['Set-Cookie'])
+#
+#
+#
+cookie1 = [{'domain': 'm.che300.com', 'httpOnly': False, 'name': 'PHPSESSID', 'path': '/', 'secure': False,
+            'value': 'e244923c55ad042e4a6951c5aabb266cde8e51e5'},
+           {'domain': 'm.che300.com', 'expiry': 1606424815, 'httpOnly': False, 'name': '_che300', 'path': '/',
+            'secure': False,
+            'value': 'R6iRutWQIVb5uBpHRDzerQwUHezAWFLJQeqiTA12UZ72v7PrF3cfuQctPmIVRt%2FyLvTy%2B6UStMDqWBHAZvTrtLxEkeHhIi53XjFHX%2Ba40bBlbMq%2FWnJHACjdrhp2UIjpGoFNA7HU5cFjFXjJOFNq8NYYCavmSinpTP9hYSLsmJ%2FRqzcbi7vB7kU58RViEOFA5lK47DjWeISpx3dZ6Vh7PfJeJ%2Fngwqi0DB3u0g0GqMG%2Fjitw8pTnXkd1MNSVMhtR%2FL6DpMItQQlXqlZM%2FvWEK%2BLt4kRoWfCL0ePqv7L%2BPhGbWSNAi5CsNI4M2kuc52nEEdyNXTmkEC0%2BCQ9j8djEY3EW3sKmozfuGWGYtmncZ7Y7NYuZ8NKheMmKL7OgsmY6m5x2uQPEPyH7xjLCsUQlsxDG%2BzG7keTHI41HTvoeCv0Q%2FgZp%2B4uRpIy8oTQ%2BkGLBPyS1AdV723TGr3%2BZ7iTnZ2aut0z435Rvk9pahUJd0Nvf1jrqO2I3JkPYdzEJDCzsO38x97dMRUX25aMeUDOnJe%2FvhfHO4q00XqriNxUgwCAO7xzesHbs6CUMzUgPY%2FKsK2WKMH6%2BPn%2FFVdMLvdQVRsMnyBkYiMi24j%2Blc4XBdTvQH66of3DU2WX%2FSjl%2B06pQc61828cf8f8458278b380667653e5b644b81078a'},
+           {'domain': 'm.che300.com', 'httpOnly': False, 'name': 'spidercodeCI12X3', 'path': '/', 'secure': False,
+            'value': '25e85c61f194c7c0c95692e065f8daaa'},
+           {'domain': '.che300.com', 'expiry': 1621933614, 'httpOnly': False, 'name': 'pcim', 'path': '/',
+            'secure': False, 'value': '46db3306c5f6096d0bc72f86d4bf1e53f270b36f'},
+           {'domain': '.che300.com', 'expiry': 1621933614, 'httpOnly': False, 'name': 'tel', 'path': '/',
+            'secure': False, 'value': '17059185578'},
+           {'domain': 'm.che300.com', 'httpOnly': False, 'name': 'spidercooskieXX12', 'path': '/', 'secure': False,
+            'value': '1606381613'},
+           {'domain': '.che300.com', 'expiry': 1621933614, 'httpOnly': False, 'name': 'device_id', 'path': '/',
+            'secure': False, 'value': 'h5a81f-3aed-0075-f145-484a'}]
+cookie2 = [{'domain': '.che300.com', 'httpOnly': False, 'name': 'Hm_lpvt_f5ec9aea58f25882d61d31eb4b353550', 'path': '/',
+            'secure': False, 'value': '1606381671'},
+           {'domain': '.m.che300.com', 'httpOnly': False, 'name': 'Hm_lpvt_12b6a0c74b9c210899f69b3429653ed6',
+            'path': '/', 'secure': False, 'value': '1606381671'},
+           {'domain': 'm.che300.com', 'httpOnly': False, 'name': 'spidercodeCI12X3', 'path': '/', 'secure': False,
+            'value': '315f2b5b7e56d1bc0a8c4e37dc1d0662'},
+           {'domain': '.m.che300.com', 'expiry': 1637917670, 'httpOnly': False,
+            'name': 'Hm_lvt_12b6a0c74b9c210899f69b3429653ed6', 'path': '/', 'secure': False, 'value': '1606381671'},
+           {'domain': '.che300.com', 'expiry': 1621933669, 'httpOnly': False, 'name': 'device_id', 'path': '/',
+            'secure': False, 'value': 'h590f1-2fcf-1d1c-b36e-e77a'},
+           {'domain': 'm.che300.com', 'httpOnly': False, 'name': 'PHPSESSID', 'path': '/', 'secure': False,
+            'value': '01c5e593a24dabe7d59b152d87419cd2743033f6'},
+           {'domain': '.che300.com', 'expiry': 1637917671, 'httpOnly': False,
+            'name': 'Hm_lvt_f5ec9aea58f25882d61d31eb4b353550', 'path': '/', 'secure': False, 'value': '1606381671'},
+           {'domain': 'm.che300.com', 'httpOnly': False, 'name': 'spidercooskieXX12', 'path': '/', 'secure': False,
+            'value': '1606381668'},
+           {'domain': 'm.che300.com', 'expiry': 1606424870, 'httpOnly': False, 'name': '_che300', 'path': '/',
+            'secure': False,
+            'value': '49ggo40vk4h3iJT5%2BLTwAHg6AxVw9Wym1Zwje%2B3EPwjrT5hhpgkguvXpJmYtr7pFE5YoT0wglOzQOd9yoZUJgxmYDIH6ufJSnlE3KnHuO53gn7ZWin%2F8Kn9%2BGIcUvOSVjDjRe0GE7lbyoQkSuaITQCFG9UC8yx4fFd2dv3lW9sLdDxiyf36llWU44fzRxYGDXN%2FQ0OHCoChxFhXlHG3x7cFXfbkqvObbhcTXSuaS72NEP4Tz3iXVfcolVEDJrRmDFEzO1UJFNE3zC4M1P6b%2FzrIrrQHtbT9Da4OF4Bi9njYg%2FH1in2FGfxJhrW72g8bFX3WeG1Q5cSx%2BO5XkX2fr5lsDkJ9HAuEyscTG5l8ME7Jjz2YsbUx%2B5temAqxTb7vj70oKbYGhUEifCdyDrpiBue8IM0ChU9PbFaFrMiY1jgJohpYTKXpV9%2BLg%2BPJ%2BLQxkqDFhClATtA5N%2BwChnxh6eCwQI6gYq5CB%2BWQqe121xvHBuGGuqWkpslbgTGEfCXGHYSM0Z1lz7PrQE8Zunkdh4TKtc%2B8HfukTPmKuE0nGFDxCKbh8efp2%2FwYy2z4bBWXtwOmH2L8jMhOQAlYfTuSNP9izX7wkdu%2B9P4ZGWaObmZ58taaoG8F%2FaJrEKxeyFCr58258945135585a5eb13c550557e41ae6d783a34f'},
+           {'domain': '.che300.com', 'expiry': 1637917671, 'httpOnly': False,
+            'name': 'zg_db630a48aa614ee784df54cc5d0cdabb', 'path': '/', 'secure': False,
+            'value': '%7B%22sid%22%3A%201606381671041%2C%22updated%22%3A%201606381671041%2C%22info%22%3A%201606381671045%2C%22superProperty%22%3A%20%22%7B%7D%22%2C%22platform%22%3A%20%22%7B%7D%22%2C%22utm%22%3A%20%22%7B%7D%22%2C%22referrerDomain%22%3A%20%22m.che300.com%22%7D'},
+           {'domain': '.che300.com', 'expiry': 1621933669, 'httpOnly': False, 'name': 'pcim', 'path': '/',
+            'secure': False, 'value': 'b72487323c369fbe694f3fbd222ac1abd1a336bf'},
+           {'domain': '.che300.com', 'expiry': 1621933669, 'httpOnly': False, 'name': 'tel', 'path': '/',
+            'secure': False, 'value': '17059177438'},
+           {'domain': '.che300.com', 'expiry': 1637917671, 'httpOnly': False, 'name': 'zg_did', 'path': '/',
+            'secure': False,
+            'value': '%7B%22did%22%3A%20%2217603cf127a25f-01658fe7a8367e-930346c-1fa400-17603cf127b248%22%7D'}]
 
-# from requests_html import HTMLSession
 
-session = requests.session()
-
-
-def getProxy():
-    # 设置代理
-    s = requests.session()
-    s.keep_alive = False
-
-    url = 'http://192.168.2.120:5000'
-    headers = {
-        'Connection': 'close',
-    }
-    proxy = s.get(url=url, headers=headers, auth=('admin', 'zd123456')).text[0:-6]
-    return proxy
+def save_cookie(cookie):
+    lst = []
+    for item in cookie:
+        nv = item['name'] + '=' + item['value']
+        lst.append(nv)
+    cookie_txt = open("che300_cookies.txt", "a")
+    cookie_txt.write('; '.join(lst) + '\n')
+    print('cookie 已保存...')
 
 
-proxy = getProxy()
-headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'zh-CN,zh;q=0.9',
-    'Connection': 'keep-alive',
-    'Cookie': '_che300=SOoVQaNf4%2BsEkXOh6Av%2BbS1FgZNx1OeY%2F8JA77%2B07xEmpi6s5k1uJG1lU5jmXrbTnBcxYCdzlu22CsfL0XQvP9%2BDLX30gtWePrSkTAE6cVBy6U3a%2FC9e%2BpnzfR4KjRmqkQOZNaDyUzNN66dIdP5dPIv63L23pkX9JO%2FGfuNVJogywprwjBv%2Bf9d9S%2BmXxuXUTcNM2km6sFWVu71rIr%2F0aR2vHJ9pBnw4T6NhvtMtiUd2VE96kKXBh%2FYmW7sXyyFhGChNeDsIKdlEQj8LHjyoGas6u9S5bjRnlSXWFGwgNA%2BzwnlqIos2Uyg39kSt1bgVZPbJq2uozTMGIqw7smO9AuokOkMbWFyjutXHyz74%2Ffmc37JzL64MYiWiV5RhJBtocT%2B9VEH0mscO5cIp0f9BkBaV2rNsrOksehWR4T%2BpRIv%2FPWzLn5B1skB64LunzffgfLRy%2BC2jmHIYQ6aRwAGlzwxwZZLflGJr8KQdn7qZJrAPSiP8lWgZuzzd%2FCbYAKnXRm7YlJ59v82iAjJH2lf7Cw9qApOTU3xnuQths11l7w4rTYW4prlnM6AAQHoMVypm578vWH8MAa%2F4W%2F5YPfuk0A%3D%3Dd6d75f981ac3c966f4e66b811fbee19be221018d; device_id=; tel=16534186105; pcim=84ceb450bcd9214ab061669659bac1fc54ba4dde; spidercooskieXX12=1606211385; spidercodeCI12X3=804b84067d9b5ec7daa7a2685890802a',
-    'Host': 'm.che300.com',
-    'Referer': 'https://m.che300.com/estimate/result/3/3/1/1/1146060/2019-3/2/1/null/2020/2018?rt=1605495570129',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'same-origin',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
-}
-url = 'http://m.che300.com/estimate/result/3/3/1/1/1146060/2019-3/2/1/null/2020/2018'
-# url = 'http://www.baidu.com'
-response = session.get(url=url, headers=headers, proxies={'http': '81.68.214.148:16128'})
-# response.html.render()
-# print(response.cookies.get_dict())
-print(response.headers['Set-Cookie'])
-
-
-
+save_cookie(cookie2)
---
+++ che300_xcx/车300selenuim注册手机号.py
@@ -0,0 +1,145 @@
+import re
+import time
+import requests
+import json
+import base64
+from PIL import Image
+import pytesseract
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class Login:
+    def __init__(self, **kwargs):
+        self.cookie_txt = open("che300_cookies.txt", "a")
+        self.chrome_options = Options()
+        # self.chrome_options.add_argument('--headless')
+        self.chrome_options.add_argument('--proxy-server=' + self.getProxy())
+        # self.chrome_options.add_argument('--proxy-server=' + '81.68.214.148:16128')
+        self.chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
+        self.chrome_options.add_argument(
+            "–Referer=https://m.che300.com/estimate/result/3/3/1/1/1168837/2020-3/0.1/1/null/2020/2018")
+        url = f'https://m.che300.com/estimate/result/3/3/1/1/1156765/2019-3/2/1/null/2020/2018'
+        self.browser = webdriver.Chrome(options=self.chrome_options)
+        self.browser.get(url)
+        self.phone = self.get_phone()
+
+    # 程序完成，自动结束程序
+    def __del__(self):
+        self.browser.close()
+        self.cookie_txt.close()
+
+    @classmethod
+    def getProxy(cls):
+        url = 'http://192.168.2.120:5000'
+        # url = 'http://120.27.216.150:5000'
+        headers = {
+            'Connection': 'close',
+        }
+        proxy = requests.get(url, headers=headers, auth=('admin', 'zd123456'), timeout=30).text[0:-6]
+        return proxy
+
+    def get_phone(self):
+        # 获取手机号
+        phone_url = 'http://45.125.46.39:8000/api/yh_qh/id=27762&operator=0&Region=0&card=0&phone=&loop=1&filer=&token=404f60e7cb3f47502753c7b627761555'
+        response = requests.get(phone_url)
+        response.encoding = response.apparent_encoding
+        try:
+            print(response.text)
+            phone = response.text.split('|')[1]
+            return phone
+        except Exception as e:
+            print('请检查接码平台...', repr(e))
+
+    def get_code(self, phone):
+        # 获取短信验证码
+        code_url = f'http://45.125.46.39:8000/api/yh_qm/id=27762&phone={phone}&t=zhongd2020&token=404f60e7cb3f47502753c7b627761555'
+        try:
+            for i in range(24):
+                response = requests.get(code_url)
+                response.encoding = response.apparent_encoding
+                text = response.text.split('|')[1]
+                print(text)
+                if '验证码为' in text:
+                    code = re.findall(r'验证码为：(.*?), 本验证码30分钟内有效', text)[0]
+                    print(code)
+                    return code
+                    break
+                else:
+                    time.sleep(5)
+        except:
+            print('请检查接码平台，取验证码出了问题。。。。')
+
+    def lh_phone(self, phone):
+        # 拉黑手机号
+        lh_url = f'http://45.125.46.39:107/api/yh_lh/id=27762&phone={phone}&token=404f60e7cb3f47502753c7b627761555'
+        response = requests.get(url=lh_url)
+        response.encoding = response.apparent_encoding
+        print(response.text)
+
+    def save_cookie(self, cookie):
+        lst = []
+        for item in cookie:
+            nv = item['name'] + '=' + item['value']
+            lst.append(nv)
+
+        self.cookie_txt.write('; '.join(lst) + '\n')
+        print('cookie 已保存...')
+
+    def start(self):
+        while 1:
+            try:
+                img_base64 = \
+                    re.findall(r'class="refresh"> <img src="data:image/png;base64,(.*?)" class="picCode"></div>',
+                               self.browser.page_source)[0]
+            except:
+                continue
+
+            self.browser.find_element_by_xpath("//input[@name='telnum']").send_keys(self.phone)
+            imgdata = base64.b64decode(img_base64)
+            file = open('yzm.png', 'wb')
+            file.write(imgdata)
+            file.close()
+            image_obj = Image.open('yzm.png')
+            img = image_obj.convert("L")  # 转灰度
+            pixdata = img.load()
+            w, h = img.size
+            threshold = 4  # 该阈值不适合所有验证码，具体阈值请根据验证码情况设置
+            # 遍历所有像素，大于阈值的为黑色
+            for y in range(h):
+                for x in range(w):
+                    if pixdata[x, y] < threshold:
+                        pixdata[x, y] = 0
+                    else:
+                        pixdata[x, y] = 255
+            yzm_text = pytesseract.image_to_string(img, lang="eng").strip().replace(' ', '')
+            print(yzm_text)
+            if len(yzm_text) < 4:
+                self.browser.find_element_by_xpath('//img[@class="refresh"]').click()
+                continue
+            else:
+                yzm_text = yzm_text[:4]
+                self.browser.find_element_by_xpath("//input[@name='check_word']").send_keys(yzm_text)
+                try:
+                    self.browser.find_element_by_xpath('//span[@class="checkword_button active"]').click()
+                except:
+                    continue
+                time.sleep(2)
+                if '秒后再试' in self.browser.page_source:
+                    code = self.get_code(self.phone)
+                    self.browser.find_element_by_xpath('//input[@placeholder="请输入验证码"]').send_keys(code)
+                    time.sleep(2)
+                    self.browser.find_element_by_xpath('//div[@class="login_button active"]').click()
+                    self.lh_phone(self.phone)
+                    time.sleep(5)
+                    cookie = self.browser.get_cookies()
+                    print(cookie)
+                    self.save_cookie(cookie)
+                    self.browser.quit()
+                    break
+
+
+if __name__ == '__main__':
+    for i in range(20):
+        Login().start()
+        time.sleep(5)
