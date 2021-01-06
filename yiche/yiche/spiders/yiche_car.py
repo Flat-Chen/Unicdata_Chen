b9fdd38063513a6d449dca41061d12c8facb2830
Flat-Chen <1316446041@qq.com>
2021-01-06 18:20:59 +0800

    易车车型库

--- yiche/yiche/spiders/yiche_car.py
+++ yiche/yiche/spiders/yiche_car.py
@@ -53,7 +53,6 @@ class YicheCarSpider(scrapy.Spider):
             yield scrapy.Request(url='http://car.yiche.com' + brand_url, headers=self.web_headers,
                                  callback=self.family_id,
                                  meta={"info": (brand_id, brand, brand_url)}, dont_filter=True)
-            break
 
     def family_id(self, response):
         brand_id, brand, brand_url = response.meta.get('info')
@@ -83,8 +82,6 @@ class YicheCarSpider(scrapy.Spider):
                                      callback=self.family_parse,
                                      meta={"info": (brand, brand_id, factory, family_name, family_id)},
                                      dont_filter=True)
-                break
-            break
 
     def family_parse(self, response):
         brand, brand_id, factory, family_name, family_id = response.meta.get('info')
@@ -92,17 +89,11 @@ class YicheCarSpider(scrapy.Spider):
         text = unquote(text)
         vehicle_ids = re.findall(r'"id":\s*(\d+),', text, re.S)
         for vehicle_id in vehicle_ids:
-            vehicle_id = '144693'
-            # print(vehicle_id)
-            # param = str({"cityId": 2401, "carId": int(vehicle_id)})
-            # print(param)
             vehicle_url = 'http://car.yiche.com/web_api/car_model_api/api/v1/car/config_new_param?cid=508&param={"cityId":2401,"carId":"' + vehicle_id + '"}'
 
             timestamp = int(time.time() * 1000)
-            sign = hashlib.md5((
-                        'cid=508&param={"cityId":2401,"carId":"' + vehicle_id + '"}19DDD1FBDFF065D3A4DA777D2D7A81EC' + str(
-                    timestamp)).encode(
-                'utf-8')).hexdigest()
+            sign = hashlib.md5(('cid=508&param={"cityId":2401,"carId":"' + vehicle_id +
+                                '"}19DDD1FBDFF065D3A4DA777D2D7A81EC' + str(timestamp)).encode('utf-8')).hexdigest()
             headers = {
                 'Host': 'car.yiche.com',
                 'x-city-id': '201',
@@ -110,56 +101,70 @@ class YicheCarSpider(scrapy.Spider):
                 'x-sign': sign,
                 'x-timestamp': timestamp,
             }
-            print(unquote(vehicle_url))
-            print(sign)
-            print(timestamp)
             yield scrapy.Request(url=vehicle_url, callback=self.vehicle_parse, headers=headers,
                                  meta={'info': (brand_id, brand, family_id, family_name, vehicle_id, factory)},
                                  dont_filter=True)
-            break
 
     def vehicle_parse(self, response):
         item = {}
         brand_id, brand, family_id, family_name, vehicle_id, factory = response.meta.get('info')
-        print(response.text)
-        # text = re.findall(r'var carCompareJson = \[(.*?)var optionalPackageJson', response.text, re.S)[0]
-        # vehicle_info = text.replace(',[[', ',[[[[').split(',[[')[0][1:-1].replace('],[', ']],[[').split('],[')
-        # print(vehicle_info)
-    #     vehicle = vehicle_info[0][1:-1].split(',')[1].replace("'", "").replace('"', '')
-    #     makeyear = vehicle_info[0][1:-1].split(',')[7].replace("'", "").replace('"', '')
-    #     guide_price = vehicle_info[1][1:-1].split(',')[0].replace("'", "").replace('"', '')
-    #     power_type = vehicle_info[1][1:-1].split(',')[-1].replace("'", "").replace('"', '')
-    #     if '电' in power_type:
-    #         displacement = vehicle_info[3][1:-1].split(',')[0].replace("'", "").replace('"', '')
-    #         maximum_cruising_range = vehicle_info[3][1:-1].split(',')[-9].replace("'", "").replace('"', '')
-    #     else:
-    #         displacement = vehicle_info[3][1:-1].split(',')[0].replace("'", "").replace('"', '')
-    #         maximum_cruising_range = None
-    #     engine = vehicle_info[1][1:-1].split(',')[4].replace("'", "").replace('"', '') + '-' + \
-    #              vehicle_info[1][1:-1].split(',')[5].replace("'", "").replace('"', '')
-    #     gearbox = vehicle_info[1][1:-1].split(',')[8].replace("'", "").replace('"', '')
-    #     environmental_standards = vehicle_info[3][1:-1].split(',')[19].replace("'", "").replace('"', '')
-    #     number_of_seats = vehicle_info[2][1:-1].split(',')[5].replace("'", "").replace('"', '')
-    #     drive_way = vehicle_info[4][1:-1].split(',')[0].replace("'", "").replace('"', '')
-    #
-    #     item['grab_time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    #     item['brand_id'] = brand_id
-    #     item['brand'] = brand
-    #     item['family_id'] = family_id
-    #     item['familyname'] = familyname
-    #     item['vehicle_id'] = vehicle_id
-    #     item['vehicle'] = str(makeyear) + ' ' + vehicle
-    #     item['makeyear'] = makeyear
-    #     item['guide_price'] = guide_price
-    #     item['power_type'] = power_type
-    #     item['engine'] = engine
-    #     item['gearbox'] = gearbox
-    #     item['environmental_standards'] = environmental_standards
-    #     item['number_of_seats'] = number_of_seats
-    #     item['displacement'] = displacement
-    #     item['drive_way'] = drive_way
-    #     item['maximum_cruising_range'] = maximum_cruising_range
-    #     item['url'] = response.url
-    #     item['status'] = str(vehicle_info)
-    #     # yield item
-    #     print(item)
+        json_data = json.loads(response.text)
+        vehicle = json_data['data'][0]['items'][0]['paramValues'][0]['value']
+        makeyear = re.findall(r'"year":"(.*?)","serialAllSpell"',
+                              json_data['data'][0]['items'][0]['paramValues'][0]['baseInfo'])[0]
+        guide_price = json_data['data'][0]['items'][1]['paramValues'][0]['value']
+        # power_type = json_data['data'][0]['items'][6]['paramValues'][0]['value']
+        try:
+            power_type = re.findall(r'"name":"动力类型","paramValues":\[{"id":.*?,"value":"(.*?)"', response.text)[0]
+        except IndexError:
+            power_type = None
+        try:
+            displacement = re.findall(r'"name":"排量.*?,"value":"(.*?)"', response.text)[0]
+        except IndexError:
+            displacement = None
+        try:
+            environmental_standards = re.findall(r'"name":"环保标准.*?,"value":"(.*?)"', response.text)[0]
+        except IndexError:
+            environmental_standards = None
+        try:
+            maximum_cruising_range = re.findall(r'"name":".*?最大续航里程.*?,"value":"(.*?)"', response.text)[0]
+        except IndexError:
+            maximum_cruising_range = None
+        try:
+            engine = re.findall(r'"name":"发动机","paramValues":\[{"id":.*?,"value":"(.*?)"', response.text)[0]
+        except IndexError:
+            engine = None
+        try:
+            gearbox = re.findall(r'"name":"变速箱类型","paramValues":\[{"id":.*?,"value":"(.*?)"', response.text)[0]
+        except IndexError:
+            gearbox = None
+        try:
+            number_of_seats = re.findall(r'"name":"座位数.*?,"value":"(.*?)"', response.text)[0]
+        except IndexError:
+            number_of_seats = None
+        try:
+            drive_way = re.findall(r'"name":"驱动形式","paramValues":\[{"id":.*?,"value":"(.*?)"', response.text)[0]
+        except IndexError:
+            drive_way = None
+        item['grab_time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        item['brand_id'] = brand_id
+        item['brand'] = brand
+        item['family_id'] = family_id
+        item['familyname'] = family_name
+        item['factory'] = factory
+        item['vehicle_id'] = vehicle_id
+        item['vehicle'] = str(makeyear) + ' ' + vehicle
+        item['makeyear'] = makeyear
+        item['guide_price'] = guide_price
+        item['power_type'] = power_type
+        item['engine'] = engine
+        item['gearbox'] = gearbox
+        item['environmental_standards'] = environmental_standards
+        item['number_of_seats'] = number_of_seats
+        item['displacement'] = displacement
+        item['drive_way'] = drive_way
+        item['maximum_cruising_range'] = maximum_cruising_range
+        item['url'] = response.url
+        item['status'] = str(response.text)
+        yield item
+        # print(item)
